d6f99145e303cec3f8e25069548c9126e2b970b9
Damian Stewart <d@damianstewart.com>
2023-10-15 11:23:03 +0200

    better handle overwriting

--- plugins/textual_inversion.py
+++ plugins/textual_inversion.py
@@ -29,6 +29,8 @@ class TextualInversionPlugin(BasePlugin):
     def on_model_load(self, **kwargs):
         ed_state: EveryDreamTrainingState = kwargs.get('ed_state')
         optimizer_config: dict = kwargs.get('optimizer_config')
+        def get_token_ids(t: str):
+            return ed_state.tokenizer.convert_tokens_to_ids(ed_state.tokenizer.tokenize(t))
 
         # check for correctly configured text encoder training
         num_te_layers = len(ed_state.text_encoder.text_model.encoder.layers)
@@ -42,7 +44,13 @@ class TextualInversionPlugin(BasePlugin):
             logging.error(f" * {Fore.LIGHTRED_EX}  {json.dumps(required_js_fragment)}{Fore.RESET}")
             raise RuntimeError("Misconfigured optimizer config")
 
-        tokens_to_add = [t['token'] for t in self.config['tokens']]
+        tokens_to_add = [t['token'] for t in self.config['tokens'] if len(get_token_ids(t['token']))>1]
+        logging.info(
+            f" * Textual inversion training adding the following tokens: {tokens_to_add}")
+        tokens_to_overwrite = [t['token'] for t in self.config['tokens'] if t['token'] not in tokens_to_add]
+        if any(tokens_to_overwrite):
+            logging.warning(f" * {Fore.LIGHTYELLOW_EX}Textual inversion training overwriting the following tokens: {tokens_to_overwrite}{Fore.RESET}")
+
         num_added_tokens = ed_state.tokenizer.add_tokens(tokens_to_add)
         if num_added_tokens != len(tokens_to_add):
             raise RuntimeError(f"Tokens not added successfully - tried to add {len(tokens_to_add)} but only added {num_added_tokens}")
@@ -53,7 +61,7 @@ class TextualInversionPlugin(BasePlugin):
         for token_info in self.config['tokens']:
             # get newly added token id
             t = token_info['token']
-            token_ids = ed_state.tokenizer.convert_tokens_to_ids(ed_state.tokenizer.tokenize(t))
+            token_ids = get_token_ids(t)
             if len(token_ids) != 1:
                 raise RuntimeError(f"Tokens not added succesfully - expected 1 token id for {t}, found {len(token_ids)}")
             token_id = token_ids[0]
@@ -61,7 +69,7 @@ class TextualInversionPlugin(BasePlugin):
 
             # copy initializer embedding
             initializer_word = token_info['initializer_word']
-            initializer_word_token_ids = ed_state.tokenizer.convert_tokens_to_ids(ed_state.tokenizer.tokenize(initializer_word))
+            initializer_word_token_ids = get_token_ids(initializer_word)
             if len(initializer_word_token_ids) != 1:
                 raise RuntimeError(f"Tokens not added succesfully - initializer word '{initializer_word}' needs "
                                    f"{len(initializer_word_token_ids)} tokens, but only single tokens are supported.")
@@ -69,14 +77,19 @@ class TextualInversionPlugin(BasePlugin):
             initializer_embedding = input_embeddings.weight.data[initializer_word_token_id]
             input_embeddings.weight.data[token_id] = initializer_embedding
 
-        self.added_token_ids = added_token_ids
+        overwriting_token_ids = [get_token_ids(t)[0] for t in tokens_to_overwrite]
+        self.training_token_ids = added_token_ids + overwriting_token_ids
         self.original_text_embeddings = ed_state.text_encoder.get_input_embeddings().weight.data.detach().clone()
 
 
     def on_step_end(self, **kwargs):
         ed_state: EveryDreamTrainingState = kwargs['ed_state']
         # reset all embeddings except the ones we're training to their original state
-        index_no_updates = torch.isin(torch.arange(len(ed_state.tokenizer)), torch.Tensor(self.added_token_ids))
+        index_no_updates = torch.isin(
+            torch.arange(len(ed_state.tokenizer)),
+            torch.Tensor(self.training_token_ids),
+            invert=True
+        )
         with (torch.no_grad()):
             ed_state.text_encoder.get_input_embeddings().weight[
                 index_no_updates
